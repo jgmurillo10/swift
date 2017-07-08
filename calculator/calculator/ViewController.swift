f0ecb01a0fb598c1a1047bec7d4ac5c478f2f84e
Juan Murillo <jg.murillo10@uniandes.edu.co>
2017-07-08 18:39:31 -0500

    calculator interface

---
+++ calculator/calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Juan Murillo on 8/07/17.
+//  Copyright © 2017 Juan Murillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
