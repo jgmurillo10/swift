ebc1e7261b9f32577ca43a88acd1155217f76b35
Juan Murillo <jg.murillo10@uniandes.edu.co>
2017-08-30 13:45:52 -0500

    Add constraints and auto layout

---
+++ Restrictions/Restrictions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Restrictions
+//
+//  Created by Juan Murillo on 30/08/17.
+//  Copyright © 2017 Juan Murillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
