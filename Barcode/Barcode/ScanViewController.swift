7d3a5701590219cb5fdba9e485a461f0e3b43b17
Juan Murillo <jg.murillo10@uniandes.edu.co>
2017-10-07 22:46:40 -0500

    Demo features

---
+++ Barcode/Barcode/ScanViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ScanViewController.swift
+//  Barcode
+//
+//  Created by Juan Murillo on 7/10/17.
+//  Copyright © 2017 Juan Murillo. All rights reserved.
+//
+
+import UIKit
+import RSBarcodes_Swift
+import AVFoundation
+class ScanViewController: RSCodeReaderViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.focusMarkLayer.strokeColor = UIColor.red.cgColor
+        
+        self.cornersLayer.strokeColor = UIColor.yellow.cgColor
+        
+        self.tapHandler = { point in
+            print(point)
+        }
+        
+        self.barcodesHandler = { barcodes in
+            for barcode in barcodes {
+                print("Barcode found: type=" + barcode.type + " value=" + barcode.stringValue)
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Barcode/Pods/Target Support Files/Pods-Barcode/Pods-Barcode-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Barcode : NSObject
+@end
+@implementation PodsDummy_Pods_Barcode
+@end
